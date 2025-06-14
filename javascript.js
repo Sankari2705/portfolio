035a11eaf06a75c462677331abb8da27325430b3
Sankari2705 <sankaridhakshna@gmail.com>
2025-06-14 08:23:54 +0530

    Profile

---
+++ javascript.js
@@ -0,0 +1,30 @@
+document.getElementById('scroll').onclick = (e) =>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  window.scrollTo(0,0);
+}
+
+let tableHead = document.getElementsByClassName('table-head');
+  let tableContents = document.getElementsByClassName('table-contents');
+  function clicktab(tabname){
+  for (tabhead of tableHead){
+    tabhead.classList.remove("active-link");
+  }
+  for (tabcontent of tableContents){
+    tabcontent.classList.remove("active-table");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-table");
+}
+
+
+let sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+
+function closemenu(){
+  sidemenu.style.right = "-180px";
+}
+
